0d206d5b855ad9e22d8d90ad91b1b0fe74cba9e8
Altmash  Siddique <altmash.siddique@digiplusit.com>
2023-12-21 13:55:01 +0530

    service order json generation changes

--- src/components/serviceOrdering/ServiceOrdering.jsx
+++ src/components/serviceOrdering/ServiceOrdering.jsx
@@ -6,22 +6,63 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Card from "./Card";
-import axios from "axios";
 import { Button } from "antd";
 import ServiceOrderJson from "./ServiceOrderJson";
+import { ECM_API_LAMBDA } from "../../constants/apiEndpoints";
+import ApiService from "../../services/apiService";
 
-const ServiceOrdering = ({ data }) => {
+const ServiceOrdering = ({ data, selectedEnvironment }) => {
   const [activeSections, setActiveSections] = useState([]);
   const [labelNamesBySection, setLabelNamesBySection] = useState([]);
   const [subSectionLabels, setSubSectionLabels] = useState([]);
-
   const [showJson, setShowJson] = useState(false);
+  const [inputValues, setInputValues] = useState({});
+  const [selectedAccordionIndexes, setSelectedAccordionIndexes] = useState([]);
+
+  const getDisplayedCardsData = () => {
+    const displayedCards = data.reduce((acc, section, index) => {
+      if (section.displayAdditionalCard && activeSections.includes(index)) {
+        const additionalCardData = {
+          title: section.title,
+          fields: labelNamesBySection[index]?.map((labelName) => ({
+            label: labelName,
+            name: labelName,
+            type: "text",
+          })) || [],
+        };
+  
+        const nestedCardCount = selectedAccordionIndexes.reduce(
+          (nestedAcc, selectedIndex) => {
+            if (
+              section &&
+              Array.isArray(section.subSections) &&
+              section.subSections.length > selectedIndex
+            ) {
+              const subSection = section.subSections[selectedIndex];
+              if (subSection) {
+                nestedAcc += subSectionLabels[selectedIndex]?.length || 0;
+              }
+            }
+            return nestedAcc;
+          },
+          0
+        );
+  
+        acc.push({ ...additionalCardData, nestedCardCount });
+      }
+      return acc;
+    }, []);
+  
+    return { displayedCards, totalDisplayedCount: displayedCards.length };
+  };
+
+  const { displayedCards, totalDisplayedCount } = getDisplayedCardsData();
+
+  const apiService = new ApiService(selectedEnvironment);
 
   const handlePrepareClick = () => {
     setShowJson(!showJson);
   };
-
-  const [inputValues, setInputValues] = useState({});
 
   const handleInputChange = (fieldName, value) => {
     setInputValues((prevValues) => ({
@@ -30,114 +71,79 @@ const ServiceOrdering = ({ data }) => {
     }));
   };
 
-  const handleAccordionChange = (index, isSubSection = false) => {
-    if (isSubSection) {
-      const subSection = data.reduce(
-        (acc, section) => acc.concat(section.subSections || []),
-        []
-      )[index];
+  const fetchData = async (title, isSubSection = false) => {
+    try {
+      const useeocApi = false;
+      const endpoint = ECM_API_LAMBDA.PATH;
+      console.log(endpoint);
+      console.log(title);
+      const params = { Parameter: title };
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "",
+      };
+      const response = await apiService.get(
+        endpoint,
+        headers,
+        params,
+        useeocApi
+      );
+      console.log(response[0]);
 
-      if (subSection && subSection.title) {
-        axios
-          .get(
-            `https://haxuqnfbm6.execute-api.eu-central-1.amazonaws.com/development?Parameter=${subSection.title}`
+      const fetchedLabelNames = response[0]?.versions[0]?.characteristics
+        .filter((characteristic) =>
+          characteristic.versions.some(
+            (version) =>
+              version.properties &&
+              version.properties.some((prop) => prop.value === "bssOrderable")
           )
-          .then((response) => {
-            const fetchedLabelNames =
-              response.data[0]?.versions[0]?.characteristics
-                .filter((characteristic) =>
-                  characteristic.versions.some(
-                    (version) =>
-                      version.properties &&
-                      version.properties.some(
-                        (prop) => prop.value === "bssOrderable"
-                      )
-                  )
-                )
-                .map((characteristic) => characteristic.id);
+        )
+        .map((characteristic) => characteristic.id);
 
-            setSubSectionLabels((prevLabels) => [
-              ...prevLabels,
-              fetchedLabelNames,
-            ]);
-          })
-          .catch((error) => {
-            console.error("Error fetching subSection data:", error);
-          });
+      if (isSubSection) {
+        setSubSectionLabels((prevLabels) => [...prevLabels, fetchedLabelNames]);
+      } else {
+        setLabelNamesBySection((prevLabels) => [
+          ...prevLabels,
+          fetchedLabelNames,
+        ]);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleAccordionChange = async (index, isSubSection = false) => {
+    if (isSubSection) {
+      const subSection = (
+        data.reduce(
+          (acc, section) => acc.concat(section.subSections || []),
+          []
+        )[index] || {}
+      ).title;
+
+      if (subSection) {
+        await fetchData(subSection, true);
       }
     } else {
+      // Logic for handling section accordion clicks
       if (activeSections.includes(index)) {
         setActiveSections(
           activeSections.filter((activeIndex) => activeIndex !== index)
         );
       } else {
         setActiveSections([...activeSections, index]);
+        
+        // Fetch section labels only when its accordion is expanded
         const section = data.find((_, idx) => idx === index);
         if (section && section.title) {
-          axios
-            .get(
-              `https://haxuqnfbm6.execute-api.eu-central-1.amazonaws.com/development?Parameter=${section.title}`
-            )
-            .then((response) => {
-              const fetchedLabelNames =
-                response.data[0]?.versions[0]?.characteristics
-                  .filter((characteristic) =>
-                    characteristic.versions.some(
-                      (version) =>
-                        version.properties &&
-                        version.properties.some(
-                          (prop) => prop.value === "bssOrderable"
-                        )
-                    )
-                  )
-                  .map((characteristic) => characteristic.id);
-
-              setLabelNamesBySection((prevLabels) => [
-                ...prevLabels,
-                fetchedLabelNames,
-              ]);
-
-              if (section.subSections) {
-                section.subSections.forEach((subSection) => {
-                  axios
-                    .get(
-                      `https://haxuqnfbm6.execute-api.eu-central-1.amazonaws.com/development?Parameter=${subSection.title}`
-                    )
-                    .then((subResponse) => {
-                      const fetchedSubLabelNames =
-                        subResponse.data[0]?.versions[0]?.characteristics
-                          .filter((characteristic) =>
-                            characteristic.versions.some(
-                              (version) =>
-                                version.properties &&
-                                version.properties.some(
-                                  (prop) => prop.value === "bssOrderable"
-                                )
-                            )
-                          )
-                          .map((characteristic) => characteristic.id);
-
-                      setSubSectionLabels((prevLabels) => [
-                        ...prevLabels,
-                        fetchedSubLabelNames,
-                      ]);
-                    })
-                    .catch((error) => {
-                      console.error("Error fetching subSection data:", error);
-                    });
-                });
-              }
-            })
-            .catch((error) => {
-              console.error("Error fetching section data:", error);
-            });
+          await fetchData(section.title);
         }
       }
     }
+    
   };
 
-  console.log(data)
-  
   return (
     <>
       <div className="container">
@@ -164,50 +170,51 @@ const ServiceOrdering = ({ data }) => {
               <AccordionDetails>
                 <Typography>{section.content}</Typography>
                 {section.subSections && (
-                  <div>
-                    {section.subSections.map((subSection, subIndex) => (
-                      <Accordion
-                        key={subIndex}
-                        onChange={() => handleAccordionChange(subIndex, true)}
+                <div>
+                  {section.subSections.map((subSection, subIndex) => (
+                    <Accordion
+                      key={subIndex}
+                      onChange={() => handleAccordionChange(subIndex, true)}
+                    >
+                      <AccordionSummary
+                        expandIcon={<ExpandMoreIcon style={{ color: "red" }} />}
+                        aria-controls={`panel${index}-${subIndex}-content`}
+                        id={`panel${index}-${subIndex}-header`}
+                        onClick={() => {
+                          if (!selectedAccordionIndexes.includes(subIndex)) {
+                            setSelectedAccordionIndexes([...selectedAccordionIndexes, subIndex]);
+                          } else {
+                            setSelectedAccordionIndexes(selectedAccordionIndexes.filter(item => item !== subIndex));
+                          }
+                        }}
                       >
-                        <AccordionSummary
-                          expandIcon={<ExpandMoreIcon />}
-                          aria-controls={`panel${index}-${subIndex}-content`}
-                          id={`panel${index}-${subIndex}-header`}
-                        >
-                          <Typography>{subSection.title}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                          <Typography>{subSection.content}</Typography>
-                          {subSection.subSubSections && (
-                            <div>
-                              {subSection.subSubSections.map(
-                                (subSubSection, subSubIndex) => (
-                                  <Accordion key={subSubIndex}>
-                                    <AccordionSummary
-                                      expandIcon={<ExpandMoreIcon />}
-                                      aria-controls={`panel${index}-${subIndex}-${subSubIndex}-content`}
-                                      id={`panel${index}-${subIndex}-${subSubIndex}-header`}
-                                    >
-                                      <Typography>
-                                        {subSubSection.title}
-                                      </Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                      <Typography>
-                                        {subSubSection.content}
-                                      </Typography>
-                                    </AccordionDetails>
-                                  </Accordion>
-                                )
-                              )}
-                            </div>
-                          )}
-                        </AccordionDetails>
-                      </Accordion>
-                    ))}
-                  </div>
-                )}
+                        <Typography>{subSection.title}</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        <Typography>{subSection.content}</Typography>
+                        {subSection.subSubSections && (
+                          <div>
+                            {subSection.subSubSections.map((subSubSection, subSubIndex) => (
+                              <Accordion key={subSubIndex}>
+                                <AccordionSummary
+                                  expandIcon={<ExpandMoreIcon style={{ color: "green" }} />}
+                                  aria-controls={`panel${index}-${subIndex}-${subSubIndex}-content`}
+                                  id={`panel${index}-${subSubIndex}-${subSubIndex}-header`}
+                                >
+                                  <Typography>{subSubSection.title}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                  <Typography>{subSubSection.content}</Typography>
+                                </AccordionDetails>
+                              </Accordion>
+                            ))}
+                          </div>
+                        )}
+                      </AccordionDetails>
+                    </Accordion>
+                  ))}
+                </div>
+              )}
               </AccordionDetails>
             </Accordion>
           ))}
@@ -306,23 +313,30 @@ const ServiceOrdering = ({ data }) => {
                   }
                   onInputChange={handleInputChange}
                 >
-                  {section.subSections &&
-                    section.subSections.map((subSection, subIndex) => (
-                      <Card
-                        key={`nestedCard-${subIndex}`}
-                        title={subSection.title}
-                        fields={
-                          subSectionLabels[subIndex]?.map(
-                            (labelName, labelIndex) => ({
-                              label: labelName,
-                              name: `field${labelIndex + 1}`,
-                              type: "text",
-                            })
-                          ) || []
-                        }
-                        onInputChange={handleInputChange}
-                      />
-                    ))}
+                  {selectedAccordionIndexes.map((selectedIndex) => {
+                    if (section && Array.isArray(section.subSections) && section.subSections.length > selectedIndex) {
+                      const subSection = section.subSections[selectedIndex];
+                      if (subSection) {
+                        return (
+                          <Card
+                            key={`nestedCard-${selectedIndex}`}
+                            title={subSection.title}
+                            fields={
+                              subSectionLabels[selectedIndex]?.map((labelName, labelIndex) => ({
+                                label: labelName,
+                                name: `field${labelIndex + 1}`,
+                                type: "text",
+                              })) || []
+                            }
+                            onInputChange={handleInputChange}
+                          />
+                        );
+                      }
+                    }
+                    return null;
+                  })}
+
+
                 </Card>
               ) : null
             )}
@@ -342,7 +356,11 @@ const ServiceOrdering = ({ data }) => {
           inputValues={inputValues} // Make sure inputValues are properly passed
           labelNamesBySection={labelNamesBySection}
           subSectionLabels={subSectionLabels}
-          serviceSpecName={data.find((_, idx) => activeSections.includes(idx))?.title}
+          serviceSpecName={
+            data.find((_, idx) => activeSections.includes(idx))?.title
+          }
+          displayedCards={displayedCards} // Pass the filtered card data
+          totalDisplayedCount={totalDisplayedCount}
         />
       )}
     </>
