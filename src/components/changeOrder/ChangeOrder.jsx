82fdc8fe6b146f4078865893537f8326ee96aa24
omkar <assassinlmao21@gmail.com>
2024-01-13 15:29:33 +0530

    change ordering enabled

--- src/components/changeOrder/ChangeOrder.jsx
+++ src/components/changeOrder/ChangeOrder.jsx
@@ -1,14 +1,80 @@
 import React, { useState } from "react";
-import { Card, Row, Col, Input, Button, Radio } from "antd";
+import {
+  Card,
+  Row,
+  Col,
+  Input,
+  Button,
+  Radio,
+  DatePicker,
+  Alert,
+  Dropdown,
+  Spin,
+  Menu,
+} from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import "./ChangeOrder.css"; // Import your CSS file
+import { SERVICE } from "../../constants/apiEndpoints";
+import ApiService from "../../services/apiService";
+import PostProcessor from "./postProcessor";
 
-const ChangeOrder = () => {
+const ChangeOrder = ({ selectedEnvironment }) => {
   const [selectedRadio, setSelectedRadio] = useState(null);
+  const [serviceId, setServiceId] = useState(null);
+  const [display, setDisplay] = useState(false);
+  const [loadingVerify, setLoadingVerify] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [srApiRes, setSrApiRes] = useState({});
+  const apiService = new ApiService(selectedEnvironment);
 
+  const handleChangeOrder = async () => {};
+  const handleDisconnectOrder = async () => {};
+  const onClose = (e) => {
+    setErrorMsg("");
+  };
   const handleRadioChange = (e) => {
     setSelectedRadio(e.target.value);
   };
+  const handleServiceIdChange = (e) => {
+    setServiceId(e.target.value);
+  };
+  const handleSubmitButtonClick = async (e) => {
+    if (selectedRadio === null) {
+      setErrorMsg("Invalid Inputs");
+    } else {
+      setLoadingVerify(true);
+      const useeocApi = true; // Adjust based on your conditions
+      const baseEndpoint = SERVICE.PATH;
+      const endpoint = baseEndpoint + "/" + serviceId;
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Basic c3ZjX2NvbXVzZXI6ZXFDU0NxPmU4Iw==",
+      };
 
+      await apiService
+        .get(endpoint, headers, { expand: true }, useeocApi)
+        .then((response) => {
+          setSrApiRes(response);
+          setLoadingVerify(false);
+          setDisplay(true);
+        })
+        .catch((error) => {
+          setErrorMsg(error.message);
+          setLoadingVerify(false);
+        });
+    }
+  };
+  const menu = (
+    <Menu>
+      <Menu.Item key="1">New line</Menu.Item>
+      <Menu.Item key="2">Change</Menu.Item>
+      <Menu.Item key="3">Disconnect</Menu.Item>
+      <Menu.Item key="4">Cancel</Menu.Item>
+      <Menu.Item key="5">Revise</Menu.Item>
+      <Menu.Item key="6">Migrate out</Menu.Item>
+      <Menu.Item key="7">New Delay</Menu.Item>
+    </Menu>
+  );
   return (
     <Card className="change-card-style">
       <div className="centered-container">
@@ -18,16 +84,38 @@ const ChangeOrder = () => {
               <h3>Service ID:</h3>
             </Col>
             <Col xs={24} sm={12} md={8} lg={11}>
-              <Input className="service-textbox" />
+              <Input
+                required
+                value={serviceId}
+                onChange={handleServiceIdChange}
+                className="service-textbox"
+              />
             </Col>
             <Col xs={24} sm={12} md={8} lg={7}>
-              <Button shape="round" className="submit-button" size="large">
+              <Button
+                disabled={
+                  selectedRadio === null
+                    ? serviceId === null
+                      ? true
+                      : false
+                    : false
+                }
+                onClick={handleSubmitButtonClick}
+                shape="round"
+                className="submit-button"
+                size="large"
+              >
                 Submit
               </Button>
             </Col>
           </Row>
         </div>
 
+        <Spin
+          style={{ display: loadingVerify ? "flex" : "none" }}
+          fullscreen
+          size="large"
+        />
         <Row gutter={[24, 24]}>
           <Radio.Group
             onChange={handleRadioChange}
@@ -47,6 +135,94 @@ const ChangeOrder = () => {
           </Radio.Group>
         </Row>
       </div>
+      <Alert
+        style={{ display: errorMsg != "" ? "flex" : "none", marginTop: "30px" }}
+        message="Error"
+        description={errorMsg}
+        type="error"
+        closable
+        onClose={onClose}
+      />
+      <Col
+        style={{
+          display: display ? (selectedRadio === 1 ? "flex" : "none") : "none",
+        }}
+      >
+        <div className="centered-container">
+          <Row gutter={[16, 16]} className="centered-container">
+            <Col xs={24} sm={24} md={24} lg={24} className="custom-col-cha">
+              <Card
+                title={
+                  srApiRes.name ? (
+                    <h3>
+                      {srApiRes.name} -{" "}
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://eoc.${selectedEnvironment}.itservices.lan/sr/#/service/${srApiRes.id}`}
+                      >
+                        {srApiRes.id}
+                      </a>
+                    </h3>
+                  ) : (
+                    ""
+                  )
+                }
+                bordered={false}
+              >
+                {/* Nested Card */}
+
+                {srApiRes.name !== null ? (
+                  <PostProcessor
+                    selectedEnvironment={selectedEnvironment}
+                    srApiRes={srApiRes}
+                  ></PostProcessor>
+                ) : (
+                  ""
+                )}
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+      <Col
+        style={{
+          display: display ? (selectedRadio === 2 ? "flex" : "none") : "none",
+        }}
+      >
+        <div className="centered-container">
+          <Row gutter={[16, 16]} className="centered-container">
+            <Col xs={24} sm={24} md={24} lg={24} className="custom-col-cha">
+              <Card title="DISCONNECT" bordered={false}>
+                {/* Nested Card */}
+                <Card bordered={false} className="async-inner-card">
+                  <Row>
+                    <Col xs={24} sm={24} md={24} lg={8} className="col-item">
+                      <h5>Error Code:</h5>
+                      <Input placeholder="Error Code" />
+                    </Col>
+                    <Col xs={24} sm={24} md={24} lg={10}>
+                      <h5>Order Note:</h5>
+                      {/* Textarea */}
+                      <Input.TextArea placeholder="Textarea" rows={4} />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col xs={24} sm={24} md={24} lg={8} className="col-item">
+                      <h5>Comment Code:</h5>
+                      <Input placeholder="Comment Code" />
+                    </Col>
+                    <Col xs={24} sm={24} md={24} lg={10}>
+                      <h5>Service Group:</h5>
+                      <Input placeholder="Service Group" />
+                    </Col>
+                  </Row>
+                </Card>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Col>
     </Card>
   );
 };
---
+++ src/components/changeOrder/postProcessor.jsx
@@ -0,0 +1,373 @@
+import { Card, Row, Col, Input, Button, Select, Spin } from "antd";
+import { DownCircleOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { json } from "./changeOrderJsons";
+import ReactJson from "react-json-view";
+import ApiService from "../../services/apiService";
+import { ECM_API_LAMBDA, SERVICE_ORDER } from "../../constants/apiEndpoints";
+const PostProcessor = ({ selectedEnvironment, srApiRes }) => {
+  const [serviceBandwidthDown, setServiceBandwidthDown] = useState(srApiRes);
+  const [serviceBandwidthUp, setServiceBandwidthUp] = useState();
+  const [promisedBandwidthDown, setPromisedBandwidthDown] = useState();
+  const [promisedBandwidthUp, setPromisedBandwidthUp] = useState();
+  const [minimumBandwidthDown, setMinimumBandwidthDown] = useState();
+  const [minimumBandwidthUp, setMimumBandwidthUp] = useState();
+  const [showJson, setShowJson] = useState(false);
+  const [jsonDiv, setJsonDiv] = useState(false);
+  const [jsonData, setJsonData] = useState({});
+  const [submitOrder, setSubmitOrder] = useState(false);
+  const [loadingVerify, setLoadingVerify] = useState(false);
+  const [changeOrderRes, setChangeOrderRes] = useState({});
+  const apiService = new ApiService(selectedEnvironment);
+
+  const selectOptions = [
+    { value: 100, label: "100" },
+    { value: 5000, label: "50000" },
+    { value: 100000, label: "100000" },
+    { value: 200000, label: "200000" },
+    { value: 500000, label: "500000" },
+    { value: 1000000, label: "1000000" },
+  ];
+  const handleShowJson = () => {
+    setShowJson(!showJson);
+  };
+  const handleserviceBandwidthDownChange = (e) => {
+    setServiceBandwidthDown(e.target.value);
+  };
+  const handleserviceBandwidthDownChangeSelect = (value) => {
+    setServiceBandwidthDown(value);
+  };
+  const handlePromisedBandwidthDownChange = (e) => {
+    setPromisedBandwidthDown(e.target.value);
+  };
+  const handlePromisedBandwidthDownChangeSelect = (value) => {
+    setPromisedBandwidthDown(value);
+  };
+  const handlePromisedBandwidthUpChange = (e) => {
+    setPromisedBandwidthUp(e.target.value);
+  };
+  const handlePromisedBandwidthUpChangeSelect = (value) => {
+    setPromisedBandwidthUp(value);
+  };
+  const handleMinimumBandwidthDownChange = (e) => {
+    setMinimumBandwidthDown(e.target.value);
+  };
+  const handleMinimumBandwidthDownChangeSelect = (value) => {
+    setMinimumBandwidthDown(value);
+  };
+  const handleMinimumBandwidthUpChange = (e) => {
+    setMimumBandwidthUp(e.target.value);
+  };
+  const handleMinimumBandwidthUpChangeSelect = (value) => {
+    setMimumBandwidthUp(value);
+  };
+  const handleserviceBandwidthUpChange = (e) => {
+    setServiceBandwidthUp(e.target.value);
+  };
+  const handleserviceBandwidthUpChangeSelect = (value) => {
+    setServiceBandwidthUp(value);
+  };
+
+  const handlePrepareClick = () => {
+    let jsonStruc = json;
+    setChangeOrderRes(null);
+    //serviceId
+    jsonStruc.orderItem[0].service.id = srApiRes.id;
+    //serviceCharacteristics
+    jsonStruc.orderItem[0].service.serviceCharacteristic[0].value =
+      serviceBandwidthUp;
+    jsonStruc.orderItem[0].service.serviceCharacteristic[1].value =
+      serviceBandwidthDown;
+    jsonStruc.orderItem[0].service.serviceCharacteristic[2].value =
+      minimumBandwidthDown;
+    jsonStruc.orderItem[0].service.serviceCharacteristic[3].value =
+      minimumBandwidthUp;
+    jsonStruc.orderItem[0].service.serviceCharacteristic[4].value =
+      promisedBandwidthUp;
+    jsonStruc.orderItem[0].service.serviceCharacteristic[5].value =
+      promisedBandwidthDown;
+    //serviceSpecification
+    jsonStruc.orderItem[0].service.serviceSpecification.id = srApiRes.name;
+    jsonStruc.orderItem[0].service.serviceSpecification.name = srApiRes.name;
+
+    //requestID
+    let randomNumber =
+      Math.floor(Math.random() * (999999999 - 100000000 + 1)) + 100000000;
+    jsonStruc.requestId = randomNumber.toString();
+    //requestedCompletionDate
+    const currentDate = new Date();
+    const tenDaysAhead = new Date(currentDate);
+    tenDaysAhead.setDate(currentDate.getDate() + 10);
+    jsonStruc.requestedCompletionDate = tenDaysAhead;
+    console.log(jsonStruc);
+    setJsonData({ jsonStruc });
+    setJsonDiv(true);
+    setSubmitOrder(true);
+  };
+
+  const handleSubmitClick = async () => {
+    setLoadingVerify(true);
+    // const useeocApi = true; // Adjust based on your conditions
+    const endpoint = SERVICE_ORDER["v1"];
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "Basic c3ZjX2NvbXVzZXI6ZXFDU0NxPmU4Iw==",
+    };
+
+    await apiService
+      .post(endpoint, jsonData.jsonStruc, headers, "")
+      .then((response) => {
+        setJsonData({ response });
+        setChangeOrderRes(response);
+        setLoadingVerify(false);
+      })
+      .catch((error) => {
+        setJsonData({ error });
+        setChangeOrderRes(error);
+        setLoadingVerify(false);
+      });
+  };
+
+  useEffect(() => {
+    console.log(srApiRes);
+    if (srApiRes.serviceCharacteristics) {
+      const serviceData = srApiRes.serviceCharacteristics;
+      const serviceCharacteristisLength = serviceData.length;
+      for (let x = 0; x < serviceCharacteristisLength; x++) {
+        if (serviceData[x].name === "minimumBandwidthDown") {
+          setMinimumBandwidthDown(serviceData[x].value);
+        } else if (serviceData[x].name === "minimumBandwidthUp") {
+          setMimumBandwidthUp(serviceData[x].value);
+        } else if (serviceData[x].name === "promisedBandwidthDown") {
+          setPromisedBandwidthDown(serviceData[x].value);
+        } else if (serviceData[x].name === "promisedBandwidthUp") {
+          setPromisedBandwidthUp(serviceData[x].value);
+        } else if (serviceData[x].name === "serviceBandwidthDown") {
+          setServiceBandwidthDown(serviceData[x].value);
+        } else if (serviceData[x].name === "serviceBandwidthUp") {
+          setServiceBandwidthUp(serviceData[x].value);
+        }
+      }
+    }
+  }, [srApiRes]);
+  return (
+    <div>
+      <Card bordered={false} className="async-inner-card">
+        <div
+          style={{
+            display:
+              srApiRes.name === "CFS_IP_ACCESS_GOP_FTTH"
+                ? "block"
+                : srApiRes.name === "CFS_IP_ACCESS_WBA_FTTH"
+                ? "block"
+                : srApiRes.name === "CFS_IP_ACCESS_WBA_VDSL"
+                ? "block"
+                : "none",
+          }}
+        >
+          <Row style={{ alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>promisedBandwidthDown</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={promisedBandwidthDown}
+                onChange={handlePromisedBandwidthDownChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={promisedBandwidthDown}
+                onChange={handlePromisedBandwidthDownChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row style={{ alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>promisedBandwidthUp</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={promisedBandwidthUp}
+                onChange={handlePromisedBandwidthUpChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={promisedBandwidthUp}
+                onChange={handlePromisedBandwidthUpChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row style={{ alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>minimumBandwidthDown</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={minimumBandwidthDown}
+                onChange={handleMinimumBandwidthDownChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={minimumBandwidthDown}
+                onChange={handleMinimumBandwidthDownChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row style={{ alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>minimumBandwidthUp</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={minimumBandwidthUp}
+                onChange={handleMinimumBandwidthUpChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={minimumBandwidthUp}
+                onChange={handleMinimumBandwidthUpChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row style={{ alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>serviceBandwidthDown</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={serviceBandwidthDown}
+                onChange={handleserviceBandwidthDownChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={serviceBandwidthDown}
+                onChange={handleserviceBandwidthDownChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row style={{ marginTop: "0vh", alignItems: "center" }}>
+            <Col style={{ marginLeft: "3vw" }} span={8}>
+              <h4>serviceBandwidthUp</h4>
+            </Col>
+            <Col span={4}>
+              <Input
+                type="number"
+                value={serviceBandwidthUp}
+                onChange={handleserviceBandwidthUpChange}
+                // className="service-textbox"
+              ></Input>
+            </Col>
+            <Col style={{ paddingLeft: "2vw" }} span={8}>
+              <Select
+                style={{ width: "80%" }}
+                value={serviceBandwidthUp}
+                onChange={handleserviceBandwidthUpChangeSelect}
+                options={selectOptions}
+              ></Select>
+            </Col>
+          </Row>
+          <Row
+            style={{
+              marginTop: "3vh",
+              marginLeft: "3vw",
+              justifyContent: "flex-start",
+            }}
+          >
+            <Col>
+              <Button
+                shape="round"
+                className="prepare-button-change"
+                onClick={handlePrepareClick}
+              >
+                Prepare Service Order
+              </Button>
+            </Col>
+
+            <Col
+              style={{
+                display: submitOrder ? "block" : "none",
+                marginLeft: "2vw",
+              }}
+            >
+              <Button
+                shape="round"
+                className="prepare-button-change"
+                onClick={handleSubmitClick}
+              >
+                Submit Service Order
+              </Button>
+            </Col>
+          </Row>
+        </div>
+      </Card>
+      <Spin
+        style={{ display: loadingVerify ? "flex" : "none" }}
+        fullscreen
+        size="large"
+      />
+      <Card
+        style={{ display: jsonDiv ? "block" : "none" }}
+        bordered={true}
+        className="async-inner-card"
+      >
+        <div style={{ marginTop: "0vh" }}>
+          <div className="card-json">
+            <h2 style={{ alignItems: "end" }}>
+              {!changeOrderRes ? (
+                <p>
+                  {" "}
+                  Generated Change Order JSON{" "}
+                  <DownCircleOutlined onClick={handleShowJson} />
+                </p>
+              ) : (
+                "Change Order Placed"
+              )}
+            </h2>
+            {showJson && (
+              <ReactJson
+                src={JSON.parse(JSON.stringify(jsonData, null, 4))}
+                theme="monokai-light"
+                style={{
+                  fontWeight: "bold",
+                  fontFamily: "monospace",
+                  letterSpacing: "1px",
+                  wordBreak: "break-word",
+                  padding: 24,
+                  width: "80%",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+export default PostProcessor;
