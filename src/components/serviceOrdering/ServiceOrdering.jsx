217b4c1ea3886a4410f07f73058bc855c3b1c0b2
Akanksha Agrawal <Akanksha.Agrawal@t-mobile.nl>
2024-01-30 11:48:34 +0530

    Feasibility data to service Ordering.jsx page

--- src/components/serviceOrdering/ServiceOrdering.jsx
+++ src/components/serviceOrdering/ServiceOrdering.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ServiceOrdering.css";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -13,7 +13,8 @@ import { ECM_API_LAMBDA, SERVICE_ORDER } from "../../constants/apiEndpoints";
 import ApiService from "../../services/apiService";
 import { toast, Toaster } from "react-hot-toast";
 import Modal from "antd/lib/modal/Modal";
-
+import { useLocation} from 'react-router-dom';
+import { Formik, Form, Field, FieldArray } from 'formik';
 const ServiceOrdering = ({ data, selectedEnvironment }) => {
   const [subSectionLabels, setSubSectionLabels] = useState([]);
   const [showJson, setShowJson] = useState(false);
@@ -31,10 +32,47 @@ const ServiceOrdering = ({ data, selectedEnvironment }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [modalCount, setModalCount] = useState(1);
   const [sectionData, setSectionData] = useState({});
-
+  const location = useLocation();
+  const {feasibilityPlaceData} = location.state || {};
+  const [localInitialValues, setLocalInitialValues] = useState({});
   const openModal = () => {
     setModalVisible(true);
   };
+
+ 
+
+  useEffect(() => {
+    if (feasibilityPlaceData && feasibilityPlaceData.installation) {
+      setLocalInitialValues(() => ({
+       ...generateFieldInitialValues('install', feasibilityPlaceData.installation),
+      }));
+    }
+
+    if (feasibilityPlaceData && feasibilityPlaceData.connectionPoint) {
+      setLocalInitialValues(() => ({
+        ...generateFieldInitialValues('connect', feasibilityPlaceData.connectionPoint),
+      }));
+    }
+    console.log('localInitialValues:', localInitialValues);
+  }, [feasibilityPlaceData]);
+
+  const generateFieldInitialValues = (prefix, data) => {
+    if (!data) {
+      return {};
+    }
+
+    return Object.keys(data).reduce((acc, key) => {
+      acc[`${prefix}_${key}`] = data[key];
+      return acc;
+    }, {});
+  };
+
+
+  
+
+ 
+ 
+
 
   const handleModalConfirm = async (index) => {
     try {
@@ -202,15 +240,15 @@ const ServiceOrdering = ({ data, selectedEnvironment }) => {
       });
     }
   };
-
+ 
   const handleInputChange = (fieldName, value) => {
     setInputValues((prevValues) => ({
       ...prevValues,
       [fieldName]: value,
     }));
-
-    console.log("Input Values service ordering:", inputValues);
   };
+
+  
 
   const fetchData = async (title, isSubSection = false) => {
     try {
@@ -421,157 +459,181 @@ const ServiceOrdering = ({ data, selectedEnvironment }) => {
           ))}
         </div>
 
-        <div className="right-section">
-          <div className="customer-id">
-            <label htmlFor="customerId">Customer ID: </label>
-            <input type="text" id="customerId" name="customerId" />
-          </div>
-
-          <div className="main-card">
-            <h2>Party Contact</h2>
-            <Card
-              title="Site Contact"
-              fields={[
-                { label: "Type", name: "site_type", type: "text" },
-                { label: "First Name", name: "site_firstname", type: "text" },
-                { label: "Last Name", name: "site_lastname", type: "text" },
-                {
-                  label: "Alternate Phone Number",
-                  name: "site_altPhNum1",
-                  type: "number",
-                },
-                { label: "Phone Number", name: "site_phNum", type: "number" },
-                {
-                  label: "Alternate Phone Number",
-                  name: "site_altPhNum2",
-                  type: "number",
-                },
-                { label: "Email", name: "site_email", type: "number" },
-              ]}
-              onInputChange={handleInputChange}
-            />
-            <Card
-              title="Installation Address"
-              fields={[
-                { label: "Type", name: "install_type", type: "text" },
-                { label: "street", name: "install_street", type: "text" },
-                {
-                  label: "houseNumber",
-                  name: "install_houseNumber",
-                  type: "number",
-                },
-                {
-                  label: "houseNumberExtension",
-                  name: "install_houseNumberExtension",
-                  type: "number",
-                },
-                { label: "postcode", name: "install_postcode", type: "number" },
-                { label: "city", name: "install_city", type: "text" },
-                { label: "country", name: "install_country", type: "text" },
-              ]}
-              onInputChange={handleInputChange}
-            />
-            <Card
-              title="Connection Point Address"
-              fields={[
-                { label: "Type", name: "connect_type", type: "text" },
-                { label: "street", name: "connect_street", type: "text" },
-                {
-                  label: "houseNumber",
-                  name: "connect_houseNumber",
-                  type: "number",
-                },
-                {
-                  label: "houseNumberExtension",
-                  name: "connect_houseNumberExtension",
-                  type: "number",
-                },
-                { label: "postcode", name: "connect_postcode", type: "number" },
-                { label: "city", name: "connect_city", type: "text" },
-                { label: "country", name: "connect_country", type: "text" },
-                {
-                  label: "connectionPointIdentifier",
-                  name: "connect_connectionPointIdentifier",
-                  type: "text",
-                },
-                { label: "nlType", name: "connect_nlType", type: "text" },
-              ]}
-              onInputChange={handleInputChange}
-            />
-
-            {data.map((section, index) =>
-              section.displayAdditionalCard &&
-              activeSections.includes(index) ? (
-                <React.Fragment key={`additionalCard-${index}`}>
-                  {section.title === "CFS_P2MP_ETH" ? (
-                    // Handle "CFS_P2MP_ETH" differently
-                    Array.from({ length: modalCount }, (_, i) => (
-                      <Card
-                        key={`additionalCard-${index}-${i}`}
-                        title={section.title}
-                        fields={
-                          sectionData[section.title]?.map((labelName) => ({
-                            label: `${section.title}.${labelName}`,
-                            name: `${section.title}.${labelName}`,
-                            type: "text",
-                          })) || []
-                        }
-                        onInputChange={handleInputChange}
-                      />
-                    ))
-                  ) : (
-                    // Keep the existing logic for other sections
+        <Formik initialValues={localInitialValues}>
+      {({ values }) => (
+              
+                  <Form>
+                  <div className="right-section">
+                    <div className="customer-id">
+                      <label htmlFor="customerId">Customer ID: </label>
+                      {/* Use Field component for Formik-managed fields */}
+                      <Field type="text" id="customerId" name="customerId" />
+                    </div>
+      
+      
+                <div className="main-card">
+                  <h2>Party Contact</h2>
+                  <Card
+                    title="Site Contact"
+                    fields={[
+                      { label: "Type", name: "site_type", type: "text" },
+                      { label: "First Name", name: "site_firstname", type: "text" },
+                      { label: "Last Name", name: "site_lastname", type: "text" },
+                      {
+                        label: "Alternate Phone Number",
+                        name: "site_altPhNum1",
+                        type: "number",
+                      },
+                      { label: "Phone Number", name: "site_phNum", type: "number" },
+                      {
+                        label: "Alternate Phone Number",
+                        name: "site_altPhNum2",
+                        type: "number",
+                      },
+                      { label: "Email", name: "site_email", type: "number" },
+                    ]}
+                    onInputChange={handleInputChange}
+                  />
+                 <FieldArray name="installationAddress">
+                 {(arrayHelpers) => (
+                        <div>
                     <Card
-                      title={section.title}
-                      fields={
-                        sectionData[section.title]?.map((labelName) => ({
-                          label: `${section.title}.${labelName}`,
-                          name: `${section.title}.${labelName}`,
-                          type: "text",
-                        })) || []
-                      }
-                      onInputChange={handleInputChange}
-                    >
-                      {section.subSections &&
-                        section.subSections.map(
-                          (subSection, subIndex) =>
-                            activeSubSections[index]?.includes(subIndex) && (
-                              <Card
-                                key={`nestedCard-${subIndex}`}
-                                title={subSection.title}
-                                fields={(
-                                  subSectionLabels[subSection.title] || []
-                                ).map((labelName, labelIndex) => ({
-                                  label: `${subSection.title}.${labelName}`,
-                                  name: `field${labelIndex + 1}`,
-                                  type: "text",
-                                }))}
-                                onInputChange={handleInputChange}
-                              />
-                            )
-                        )}
-                    </Card>
-                  )}
-                </React.Fragment>
-              ) : null
-            )}
+                      title="Installation Address"
+                      fields={[
+                        { label: "Type", name: "install_type", type: "text" },
+                        { label: "street", name: "install_street", type: "text" },
+                        {
+                          label: "houseNumber",
+                          name: "install_houseNumber",
+                          type: "number",
+                        },
+                        {
+                          label: "houseNumberExtension",
+                          name: "install_houseNumberExtension",
+                          type: "number",
+                        },
+                        { label: "postcode", name: "install_postcode", type: "number" },
+                        { label: "city", name: "install_city", type: "text" },
+                        { label: "country", name: "install_country", type: "text" },
+                      ]}
+                    />
+                      </div>
+                )}
+              </FieldArray>
+                    <FieldArray name="connectionPointAddresses">
+                    {(arrayHelpers) => (
+                  <div>
+                          <Card
+                            title="Connection Point Address"
+                            fields={[
+                              { label: "Type", name: "connect_type", type: "text" },
+                              { label: "street", name: "connect_street", type: "text" },
+                              {
+                                label: "houseNumber",
+                                name: "connect_houseNumber",
+                                type: "number",
+                              },
+                              {
+                                label: "houseNumberExtension",
+                                name: "connect_houseNumberExtension",
+                                type: "number",
+                              },
+                              { label: "postcode", name: "connect_postcode", type: "number" },
+                              { label: "city", name: "connect_city", type: "text" },
+                              { label: "country", name: "connect_country", type: "text" },
+                              {
+                                label: "connectionPointIdentifier",
+                                name: "connect_connectionPointIdentifier",
+                                type: "text",
+                              },
+                              { label: "nlType", name: "connect_nlType", type: "text" },
+                            ]}
+                          />
+                        </div>
+                      )}
+                    </FieldArray>
+                    {console.log('Formik values:', values)}
 
-            <Button
-              shape="round"
-              className="prepare-button"
-              onClick={handlePrepareClick}
-            >
-              Prepare Service Order
-            </Button>
-            <Button
-              shape="round"
-              className="prepare-button"
-              onClick={handleSubmitClick}
-            >
-              Submit Service Order
-            </Button>
-          </div>
-        </div>
+            
+                  {data.map((section, index) =>
+                    section.displayAdditionalCard &&
+                    activeSections.includes(index) ? (
+                      <React.Fragment key={`additionalCard-${index}`}>
+                        {section.title === "CFS_P2MP_ETH" ? (
+                          // Handle "CFS_P2MP_ETH" differently
+                          Array.from({ length: modalCount }, (_, i) => (
+                            <Card
+                              key={`additionalCard-${index}-${i}`}
+                              title={section.title}
+                              fields={
+                                sectionData[section.title]?.map((labelName) => ({
+                                  label: `${section.title}.${labelName}`,
+                                  name: `${section.title}.${labelName}`,
+                                  type: "text",
+                                })) || []
+                              }
+                              onInputChange={handleInputChange}
+                            />
+                          ))
+                        ) : (
+                          // Keep the existing logic for other sections
+                          <Card
+                            title={section.title}
+                            fields={
+                              sectionData[section.title]?.map((labelName) => ({
+                                label: `${section.title}.${labelName}`,
+                                name: `${section.title}.${labelName}`,
+                                type: "text",
+                              })) || []
+                            }
+                            onInputChange={handleInputChange}
+                          >
+                            {section.subSections &&
+                              section.subSections.map(
+                                (subSection, subIndex) =>
+                                  activeSubSections[index]?.includes(subIndex) && (
+                                    <Card
+                                      key={`nestedCard-${subIndex}`}
+                                      title={subSection.title}
+                                      fields={(
+                                        subSectionLabels[subSection.title] || []
+                                      ).map((labelName, labelIndex) => ({
+                                        label: `${subSection.title}.${labelName}`,
+                                        name: `field${labelIndex + 1}`,
+                                        type: "text",
+                                      }))}
+                                      onInputChange={handleInputChange}
+                                    />
+                                  )
+                              )}
+                          </Card>
+                        )}
+                      </React.Fragment>
+                    ) : null
+                  )}
+      
+                  <Button
+                    shape="round"
+                    className="prepare-button"
+                    onClick={handlePrepareClick}
+                  >
+                    Prepare Service Order
+                  </Button>
+                  <Button
+                    shape="round"
+                    className="prepare-button"
+                    onClick={handleSubmitClick}
+                  >
+                    Submit Service Order
+                  </Button>
+                </div>
+              </div>
+              </Form>
+                )
+              }
+           </Formik>
+
+       
       </div>
       {showJson && (
         <ServiceOrderJson
@@ -593,6 +655,7 @@ const ServiceOrdering = ({ data, selectedEnvironment }) => {
           selectedAccordionIndexes={selectedAccordionIndexes}
           handleJsonData={handleJsonData}
           selectedVersion={selectedVersion}
+          feasibilityPlaceData = {feasibilityPlaceData}
         />
       )}
       {modalContent}
